6baf582af4e426060813f96f05fdd4790235c037
cristian-ig <cristian-ig@users.noreply.github.com>
2018-10-11 00:19:52 +0300

    Add files via upload

--- blueBot.py
+++ blueBot.py
@@ -1,4 +1,5 @@
 import aiml
+import ast
 import re
 
 kernel = aiml.Kernel()
@@ -104,6 +105,269 @@ def enumerareRestante():
         print("Am rastanta la "+str(materii_restante))
     else:
         print("Nu am nici o restanta")
+
+def worstbestgrade(message):
+    message.upper()
+    if message == "_CEAMAIMICANOTADINFIECARESEMESTRU_":
+        max1= 11
+        max2= 11
+        max3= 11
+        max4= 11
+        materi1 = None
+        materi2 = None
+        materi3 = None
+        materi4 = None
+
+        for materie,semestru in materii_semestru.items():
+            if semestru is 1:
+                if materii2note[materie]< max1:
+                    max1 = materii2note[materie]
+                    materi1 = materie
+            elif semestru is 2:
+                if materii2note[materie] < max2:
+                    max2 = materii2note[materie]
+                    materi2 = materie
+            elif semestru is 3:
+                if materii2note[materie] < max3:
+                    max3 = materii2note[materie]
+                    materi3 = materie
+            elif semestru is 4:
+                if materii2note[materie] < max4:
+                    max4 = materii2note[materie]
+                    materi4 = materie
+        print("In semestrul 1 am avut nota " + str(max1) + " la materia " + str(
+            materi1) + ", in semestrul 2 am avut nota " + str(max2) + " la " + str(materi2) + ", in semestrul 3 " + str(
+            max3) + " la " + str(materi3) + "iar in semestrul 4 " + str(max4) + " la " + str(materi4))
+        pass
+    elif message == "_CEAMAIMARENOTADINFIECARESEMESTRU_":
+        max1= -10
+        max2= -10
+        max3= -10
+        max4= -10
+        materi1 = None
+        materi2 = None
+        materi3 = None
+        materi4 = None
+
+        for materie,semestru in materii_semestru.items():
+            if semestru is 1:
+                if materii2note[materie]> max1:
+                    max1 = materii2note[materie]
+                    materi1 = materie
+            elif semestru is 2:
+                if materii2note[materie] > max2:
+                    max2 = materii2note[materie]
+                    materi2 = materie
+            elif semestru is 3:
+                if materii2note[materie] > max3:
+                    max3 = materii2note[materie]
+                    materi3 = materie
+            elif semestru is 4:
+                if materii2note[materie] > max4:
+                    max4 = materii2note[materie]
+                    materi4 = materie
+        print("In semestrul 1 am avut nota " + str(max1) + " la materia " + str(
+            materi1) + ", in semestrul 2 am avut nota " + str(max2) + " la " + str(materi2) + ", in semestrul 3 " + str(
+            max3) + " la " + str(materi3) + "iar in semestrul 4 " + str(max4) + " la " + str(materi4))
+        pass
+    elif message == "_CEAMAIMICANOTADINFIECAREAN_":
+        max1 = 11
+        max2 = 11
+        materie1 = None
+        materie2 = None
+        for materie,an in materii_an.items():
+            if an is 1:
+                if materii2note[materie]<max1:
+                    max1 = materii2note[materie]
+                    materie1 = materie
+            else:
+                if materii2note[materie]<max2:
+                    max2= materii2note[materie]
+                    materie2 = materie
+        print("In primul an am luat "+str(max1)+" la "+str(materie1)+" iar in anul 2 "+str(max2)+" la "+str(materie2))
+        pass
+    elif message == "_CEAMAIMARENOTADINFIECAREAN_":
+        max1 = -11
+        max2 = -11
+        materie1 = None
+        materie2 = None
+        for materie, an in materii_an.items():
+            if an is 1:
+                if materii2note[materie] >max1:
+                    max1 = materii2note[materie]
+                    materie1 = materie
+            else:
+                if materii2note[materie] > max2:
+                    max2 = materii2note[materie]
+                    materie2 = materie
+        print(
+            "In primul an am luat " + str(max1) + " la " + str(materie1) + " iar in anul 2 " + str(max2) + " la " + str(
+                materie2))
+        pass
+
+def worstbestobject(message):
+    message.upper()
+    if message == "_MATERIACUNOTACEAMAIMICADINFIECAREAN_":
+        materia1 = None
+        materia2 = None
+
+        max1 = 11
+        max2 = 11
+        for materie,an in materii_an.items():
+            if an is 1:
+                if materii2note[materie]<max1:
+                    max1 = materii2note[materie]
+                    materia1 = materie
+            else:
+                if materii2note[materie]<max2:
+                    max2 = materii2note[materie]
+                    materia2 = materie
+        print ("Am avut "+str(max1)+" la "+str(materia1)+" in primul an"+" iar in anul 2 "+str(max2)+" la "+str(materia2))
+        pass
+    elif message == "_MATERIACUNOTACEAMAIMAREDINFIECAREAN_":
+        materia1 = None
+        materia2 = None
+
+        max1 = 11
+        max2 = 11
+        for materie, an in materii_an.items():
+            if an is 1:
+                if materii2note[materie] > max1:
+                    max1 = materii2note[materie]
+                    materia1 = materie
+            else:
+                if materii2note[materie] > max2:
+                    max2 = materii2note[materie]
+                    materia2 = materie
+        print("Am avut " + str(max1) + " la " + str(materia1) + " in primul an" + " iar in anul 2 " + str(
+            max2) + " la " + str(materia2))
+        pass
+    elif message == "_MATERIACUNOTACEAMAIMICADINFIECARESEMESTRU_":
+        max1 = 11
+        max2 = 11
+        max3 = 11
+        max4 = 11
+        materi1 = None
+        materi2 = None
+        materi3 = None
+        materi4 = None
+
+        for materie, semestru in materii_semestru.items():
+            if semestru is 1:
+                if materii2note[materie] < max1:
+                    max1 = materii2note[materie]
+                    materi1 = materie
+            elif semestru is 2:
+                if materii2note[materie] < max2:
+                    max2 = materii2note[materie]
+                    materi2 = materie
+            elif semestru is 3:
+                if materii2note[materie] < max3:
+                    max3 = materii2note[materie]
+                    materi3 = materie
+            elif semestru is 4:
+                if materii2note[materie] < max4:
+                    max4 = materii2note[materie]
+                    materi4 = materie
+        print("In semestrul 1 am avut nota " + str(max1) + " la materia " + str(
+            materi1) + ", in semestrul 2 am avut nota " + str(max2) + " la " + str(materi2) + ", in semestrul 3 " + str(
+            max3) + " la " + str(materi3) + "iar in semestrul 4 " + str(max4) + " la " + str(materi4))
+        pass
+        pass
+    elif message == "_MATERIACUNOTACEAMAIMAREDINFIECARESEMESTRU_":
+        max1 = -10
+        max2 = -10
+        max3 = -10
+        max4 = -10
+        materi1 = None
+        materi2 = None
+        materi3 = None
+        materi4 = None
+
+        for materie, semestru in materii_semestru.items():
+            if semestru is 1:
+                if materii2note[materie] > max1:
+                    max1 = materii2note[materie]
+                    materi1 = materie
+            elif semestru is 2:
+                if materii2note[materie] > max2:
+                    max2 = materii2note[materie]
+                    materi2 = materie
+            elif semestru is 3:
+                if materii2note[materie] > max3:
+                    max3 = materii2note[materie]
+                    materi3 = materie
+            elif semestru is 4:
+                if materii2note[materie] > max4:
+                    max4 = materii2note[materie]
+                    materi4 = materie
+        print("In semestrul 1 am avut nota " + str(max1) + " la materia " + str(
+            materi1) + ", in semestrul 2 am avut nota " + str(max2) + " la " + str(materi2) + ", in semestrul 3 " + str(
+            max3) + " la " + str(materi3) + "iar in semestrul 4 " + str(max4) + " la " + str(materi4))
+        pass
+
+def processSentence(sentence):
+    mistakesFile = open("mistakes.dict",'r')
+    mistakes = ast.literal_eval(mistakesFile.readline())
+
+    correctFile = open("correct.dict",'r')
+    correct = ast.literal_eval(correctFile.readline())
+
+    mistakesFile.close()
+    correctFile.close()
+
+    words = re.split('\s,|\s\\.|\s',sentence)
+    print("WORDS "+str(words))
+    finalSentence = ""
+    for word in words:
+        word = word.upper()
+        print("WORD:"+word)
+        if word not in correct:
+            if word not in mistakes.keys():
+                print("What did you mean by "+str(word)+"?")
+                rsp = input("Please type one word :) >>>")
+                rsp_split = re.split('\s,|\s\\.|\s',rsp)
+                if len(rsp_split) > 1 or rsp_split[0] is '' :
+                    while len(rsp_split) is not 1:
+                        print("You did not type one word!")
+                        rsp = input("Please type one word :) >>>")
+                        rsp_split = re.split('\s,|\s\\.|\s', rsp)
+                print("Was that a mistake?")
+                resp = input("Yes/No ")
+                resp = resp.upper()
+
+                if resp == "YES":
+                    mistakes[word] = rsp[:]
+                elif resp == "NO":
+                    pass
+                else:
+                    while resp !="YES" or resp !="NO":
+                        resp = input("Yes/No ")
+                        resp = resp.upper()
+
+                        if resp =="YES":
+                            mistakes[word]=rsp[:].upper()
+                        elif resp == "NO":
+                           pass
+
+                correct.append(rsp[:].upper())
+                word = rsp
+            else:
+                word = mistakes[word]
+        finalSentence = finalSentence+" "+word
+        finalSentence = finalSentence.upper()
+
+
+    mistakesFile = open("mistakes.dict", 'w')
+    mistakesFile.write(str(mistakes))
+    mistakesFile.close()
+
+    correctFile = open("correct.dict", 'w')
+    correctFile.write(str(correct))
+    correctFile.close()
+
+    return finalSentence
+
 # def learnName(message):
 #     message.upper()
 #     regex = re.compile("^(Sunt)\\ \\w")
@@ -114,33 +378,49 @@ def enumerareRestante():
 
 
 
-kernel.learn("botlogic.xml")
+def main():
+    kernel.learn("botlogic.xml")
+    file = open("mistakes.dict","r+")
+    r = file.readline()
+    d = ast.literal_eval(r)
+    print(d.keys())
+    file.close()
 
-while True:
-    internalResponse =False
+    while True:
+        internalResponse =False
 
-    message = input("Message >>> ")
-    response = kernel.respond(message)
-    print("response: "+response)
+        message = input("Message >>> ")
+        message = processSentence(message)
+        response = kernel.respond(message)
+        response = response.upper()
 
-    if response == "_INTEGRALIST_":
-        calculateIntegralist()
-        internalResponse=True
-    elif response == "_RESTANTEANI_":
-        calculateAniRestante()
-        internalResponse=True
-    elif response == "_RESTANTESEM_":
-        calculateRestanteSem()
-        internalResponse=True
-    elif response == "_MATERIIPREFERATE_":
-        materiiPreferate()
-        internalResponse=True
-    elif response == "_MATERIIANCURENT_":
-        materiiAnCurent()
-        internalResponse=True
-    elif response == "_ENUMERARERESTANTE_":
-        enumerareRestante()
-        internalResponse=True
+        print("response: "+response)
 
-    if not internalResponse:
-        print()
+        if response == "_INTEGRALIST_":
+            calculateIntegralist()
+            internalResponse=True
+        elif response == "_RESTANTEANI_":
+            calculateAniRestante()
+            internalResponse=True
+        elif response == "_RESTANTESEM_":
+            calculateRestanteSem()
+            internalResponse=True
+        elif response == "_MATERIIPREFERATE_":
+            materiiPreferate()
+            internalResponse=True
+        elif response == "_MATERIIANCURENT_":
+            materiiAnCurent()
+            internalResponse=True
+        elif response == "_ENUMERARERESTANTE_":
+            enumerareRestante()
+            internalResponse=True
+        elif response.upper() == "_CEAMAIMICANOTADINFIECARESEMESTRU_" or response == "_CEAMAIMARENOTADINFIECARESEMESTRU_" or response == "_CEAMAIMICANOTADINFIECARESAN_" or response == "_CEAMAIMARENOTADINFIECAREAN_":
+            worstbestgrade(response)
+        elif response == "_MATERIACUNOTACEAMAIMICADINFIECAREAN_" or response == "_MATERIACUNOTACEAMAIMAREDINFIECAREAN_" or response == "_MATERIACUNOTACEAMAIMICADINFIECARESEMESTRU_"  or response == "_MATERIACUNOTACEAMAIMAREDINFIECARESEMESTRU_":
+            worstbestobject(response)
+
+        if not internalResponse:
+            print()
+
+if __name__ == "__main__":
+    main()
